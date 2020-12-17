c0f6770b067ae004e7159e306568942dcff60e1b
Phoenix <zengphoenix@live.com>
2020-12-17 19:44:42 +0800

    修复color逗号的错误 添加css语法识别

--- README.md
+++ README.md
@@ -4,17 +4,18 @@
 
 使得魔兽争霸3的字符串前缀可以被颜色选择器识别和修改
 
-支持以下两种语法
+支持以下语法
 
 ``` txt
     |cffffffff|r
     color(255,255,255,255)
+    #ffffff
 ```
 
 目前支持以下格式的文件
 
 ``` txt
-    "jass","lua","ini","vjass","zinc","fdf","json"
+    "jass","lua","ini","vjass","zinc","fdf","json","html","css","xml"
 ```
 
 群:692125060
--- src/main/color-provider.ts
+++ src/main/color-provider.ts
@@ -13,6 +13,16 @@ import * as vscode from 'vscode';
 const convertInt2Hex = (int: number) => {
   return Math.ceil(int * 255).toString(16).padStart(2, "0");
 };
+const color2CssColorCode = (color: vscode.Color) => {
+  if (color instanceof vscode.Color) {
+    let r = color.red;
+    let g = color.green;
+    let b = color.blue;
+    let colorCodeString =  convertInt2Hex(r) + convertInt2Hex(g) + convertInt2Hex(b);
+    return colorCodeString;
+  }
+  return "000000";
+};
 const color2JColorCode = (color: vscode.Color) => {
   if (color instanceof vscode.Color) {
     let r = color.red;
@@ -33,7 +43,7 @@ const color2ColorClassColorCode = (color: vscode.Color) => {
     let colorCodeString = ""+Math.ceil(r * 255)+","+Math.ceil(g * 255)+","+Math.ceil(b * 255)+","+Math.ceil(a * 255);
     return colorCodeString;
   }
-  return "0,0,0,0";
+  return "0, 0, 0, 0";
 };
 
 class HexDocumentColorProvider implements vscode.DocumentColorProvider {
@@ -82,20 +92,63 @@ class HexDocumentColorProvider implements vscode.DocumentColorProvider {
   }
 }
 
-class ColorDocumentColorProvider implements vscode.DocumentColorProvider {
+
+class CssDocumentColorProvider implements vscode.DocumentColorProvider {
 
   /// 颜色改变到文档
   provideDocumentColors(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.ColorInformation[]> {
     let lineCount = document.lineCount;
     let colors = new Array<vscode.ColorInformation>();
-    let colorReg = new RegExp(/color\(((\d+)(,\d+){3})\)/, "g");
+    let colorReg = new RegExp(/#[\da-fA-F]{6}/, "g");
     for (let i = 0; i < lineCount; i++) {
       let lineText = document.lineAt(i).text;
       let colotSet = lineText.match(colorReg);
       let posstion = 0;
       if (colotSet) {
         colotSet.forEach(x => {
-          let nums = x.match(new RegExp(/(\d+),(\d+),(\d+),(\d+)/));
+          posstion = lineText.indexOf(x, posstion);
+          let range = new vscode.Range(i, posstion, i, posstion + x.length);
+          let a = 255 / 255;
+          let r = Number.parseInt("0x" + lineText.substr(posstion + 1, 2)) / 255;
+          let g = Number.parseInt("0x" + lineText.substr(posstion + 3, 2)) / 255;
+          let b = Number.parseInt("0x" + lineText.substr(posstion + 5, 2)) / 255;
+          colors.push(new vscode.ColorInformation(range, new vscode.Color(r, g, b, a)));
+          posstion += x.length;
+        });
+      }
+    }
+    return colors;
+  }
+  /// 文档改变到颜色
+  provideColorPresentations(color: vscode.Color, context: { document: vscode.TextDocument; range: vscode.Range; }, token: vscode.CancellationToken): vscode.ProviderResult<vscode.ColorPresentation[]> {
+    let r = color.red;
+    let g = color.green;
+    let b = color.blue;
+    let a = color.alpha;
+    let document = context.document;
+    let range = context.range;
+    let documentText = document.getText(range);
+    return [new vscode.ColorPresentation(`#${
+      color2CssColorCode(new vscode.Color(r, g, b, a))
+    }`)]
+  }
+}
+
+
+class ColorDocumentColorProvider implements vscode.DocumentColorProvider {
+
+  /// 颜色改变到文档
+  provideDocumentColors(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.ColorInformation[]> {
+    let lineCount = document.lineCount;
+    let colors = new Array<vscode.ColorInformation>();
+    let colorReg = new RegExp(/color\( *((\d+)( *, *\d+ *){3})\)/, "g");
+    for (let i = 0; i < lineCount; i++) {
+      let lineText = document.lineAt(i).text;
+      let colotSet = lineText.match(colorReg);
+      let posstion = 0;
+      if (colotSet) {
+        colotSet.forEach(x => {
+          let nums = x.match(new RegExp(/ *(\d+) *, *(\d+) *, *(\d+) *, *(\d+) */));
           posstion = lineText.indexOf(x, posstion);
           let range = new vscode.Range(i, posstion, i, posstion + x.length);
           let r = Number.parseInt(nums?nums[1]:"255") / 255;
@@ -126,10 +179,14 @@ class ColorDocumentColorProvider implements vscode.DocumentColorProvider {
 
 const hexSupportLanguages = ["jass","lua","ini","vjass","zinc","fdf","json"];
 const colorSupportLanguages = ["lua","vjass","zinc"];
+const cssSupportLanguages = ["css","html","xml"];
 
 hexSupportLanguages.forEach(language=>{
   vscode.languages.registerColorProvider(language, new HexDocumentColorProvider);
 });
 colorSupportLanguages.forEach(language=>{
   vscode.languages.registerColorProvider(language, new ColorDocumentColorProvider);
+});
+cssSupportLanguages.forEach(language=>{
+  vscode.languages.registerColorProvider(language, new CssDocumentColorProvider);
 });
